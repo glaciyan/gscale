fcb39c28ce25eee39054dba9078081512e0dc89b
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-05-29 13:58:17 +0200

    too strict

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode, useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
-import cn from "classnames";
 
 type Props = {
     children?: ReactNode;
