b6af733838b36e5ee712a957fa0a7c5184aef2be
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-02 14:25:20 +0200

    ascension cost function

--- lib/index.ts
+++ lib/index.ts
@@ -1,48 +1,34 @@
-import { Levels } from "../data/characterLevels";
-import { costs as XpMoreCosts } from "../data/characterLevels";
-
 export function toId(name: string) {
     return name.replace(/'/g, "").replace(/[ -]/g, "_").toLowerCase();
 }
 
-export function additionOrCreate(
+export function sumObjectArray(
     array: any[],
-    order: number,
-    id: any,
-    amount: number | null
+    distinction: string,
+    numberField: string
 ) {
-    if (amount === null) return;
-    const sameIdEntry = array.find((entry) => entry.id === id);
+    const out: any[] = [];
+    array.forEach((arrayItem) => {
+        const match = out.find(
+            (value) => value[distinction] === arrayItem[distinction]
+        );
 
-    if (sameIdEntry !== undefined) {
-        sameIdEntry.amount += amount;
-    } else {
-        array.push({ id: id, order: order, amount: amount });
-    }
+        if (match) {
+            match[numberField] += arrayItem[numberField];
+        } else {
+            out.push(arrayItem);
+        }
+    });
+
+    return out;
 }
 
-export function totalXpAndMora(level: Levels) {
-    return XpMoreCosts.reduce(
-        (accumulator, stage) => {
-            if (level >= stage.level) {
-                return {
-                    level: stage.level,
-                    xp: accumulator.xp + stage.xp,
-                    mora: accumulator.mora + stage.mora,
-                };
-            } else return accumulator;
-        },
-        { level: 1, xp: 0, mora: 0 }
-    );
-}
-
-export function getAscensionLevel(level: Levels) {
-    if (level <= 20) return -1;
-    else if (level >= 21 && level <= 40) return 0;
-    else if (level >= 41 && level <= 50) return 1;
-    else if (level >= 51 && level <= 60) return 2;
-    else if (level >= 61 && level <= 70) return 3;
-    else if (level >= 71 && level <= 80) return 4;
-    else if (level >= 81) return 5;
-    else return -1;
+export function getAscensionLevel(level: number): number {
+    if (level >= 21 && level <= 40) return 1;
+    else if (level >= 41 && level <= 50) return 2;
+    else if (level >= 51 && level <= 60) return 3;
+    else if (level >= 61 && level <= 70) return 4;
+    else if (level >= 71 && level <= 80) return 5;
+    else if (level >= 81) return 6;
+    else return 0;
 }
