d05dd022ef6fe7b5c13ad88bc32f3c45ab1299ca
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-04 10:39:15 +0200

    added new entries in database and sorted fields

--- lib/buildsDatabase.ts
+++ lib/buildsDatabase.ts
@@ -2,8 +2,8 @@ import dexie from "dexie";
 
 const buildsDB = new dexie("buildsDB");
 
-buildsDB.version(2).stores({
-    builds: "++id, type, characterId, level, normal, elemental, burst, completed",
+buildsDB.version(1).stores({
+    builds: "++id, &order, completed, type, characterId, level, normal, elemental, burst",
 });
 
 export default buildsDB as any;
