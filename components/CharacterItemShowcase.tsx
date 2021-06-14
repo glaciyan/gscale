fe4579da5312fd7c8c6bee6367df64e4e4856037
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-14 19:45:40 +0200

    fixed item showcase title

--- components/CharacterItemShowcase.tsx
+++ components/CharacterItemShowcase.tsx
@@ -17,34 +17,41 @@ export const CharacterItemShowcase: React.FC<CharacterItemShowcaseProps> = ({
 }) => {
     const ci = new ItemGen(character);
 
+    const local = ci.local(1).name;
+    const common = ci.common(1, 1).name;
+    const boss = ci.boss(1).name;
+    const gem = ci.gem(5, 1).name;
+    const book = ci.book(4, 1).name;
+    const weekly = ci.weekly(1).name;
+
     return (
         <div className="hidden py-6 border-t-2 border-gscale-dark-background-primary sm:block">
             <ShowcaseRow>
                 <MiniItemCard
-                    imageUrl={`/images/materials/${toId(ci.local(1).name)}.png`}
-                    imageName="common"
+                    imageUrl={`/images/materials/${toId(local)}.png`}
+                    imageName={local}
                 />
                 <MiniItemCard
-                    imageUrl={`/images/materials/${toId(ci.common(1, 1).name)}.png`}
-                    imageName="common"
+                    imageUrl={`/images/materials/${toId(common)}.png`}
+                    imageName={common}
                 />
                 <MiniItemCard
-                    imageUrl={`/images/materials/${toId(ci.boss(1).name)}.png`}
-                    imageName="common"
+                    imageUrl={`/images/materials/${toId(boss)}.png`}
+                    imageName={boss}
                 />
             </ShowcaseRow>
             <ShowcaseRow className="flex">
                 <MiniItemCard
-                    imageUrl={`/images/materials/${toId(ci.gem(5, 1).name)}.png`}
-                    imageName="common"
+                    imageUrl={`/images/materials/${toId(gem)}.png`}
+                    imageName={gem}
                 />
                 <MiniItemCard
-                    imageUrl={`/images/materials/${toId(ci.book(4, 1).name)}.png`}
-                    imageName="common"
+                    imageUrl={`/images/materials/${toId(book)}.png`}
+                    imageName={book}
                 />
                 <MiniItemCard
-                    imageUrl={`/images/materials/${toId(ci.weekly(1).name)}.png`}
-                    imageName="common"
+                    imageUrl={`/images/materials/${toId(weekly)}.png`}
+                    imageName={weekly}
                 />
             </ShowcaseRow>
         </div>
