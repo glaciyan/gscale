6fa948e7a6fbb3eddc5dbbd3c01beb568d220007
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-16 17:18:43 +0200

    using normal button over div

--- components/Button.tsx
+++ components/Button.tsx
@@ -28,12 +28,12 @@ export const Button: React.FC<ButtonProps> = ({
     fullw,
     isLoading,
 }) => {
-    let cn: string = `buttoncommon shadow-md w-max text-gscale-dark-text-primary bg-opacity-70 bg-${color} ${className} transition-colors hover:bg-opacity-50 active:bg-opacity-30 hover:shadow-lg select-none`;
+    let cn: string = `buttoncommon shadow-md w-max text-gscale-dark-text-primary bg-opacity-70 bg-${color} ${className} transition-colors hover:bg-opacity-50 active:bg-opacity-30 hover:shadow-lg select-none focus-visible:ring focus:outline-none`;
 
     if (secondary) {
-        cn = `buttoncommon ring-1 ring-opacity-80 text-${color} ring-${color} ${className} bg-black transition-colors bg-opacity-0 hover:bg-opacity-20 active:bg-opacity-50 select-none`;
+        cn = `buttoncommon ring-1 ring-opacity-80 text-${color} ring-${color} ${className} bg-black transition-colors bg-opacity-0 hover:bg-opacity-20 active:bg-opacity-50 select-none focus-visible:bg-opacity-20`;
     } else if (ternary) {
-        cn = `buttoncommon w-max text-${color} bg-black transition-colors bg-opacity-0 hover:bg-opacity-20 active:bg-opacity-50 select-none ${className}`;
+        cn = `buttoncommon w-max text-${color} bg-black transition-colors bg-opacity-0 hover:bg-opacity-20 active:bg-opacity-50 select-none focus-visible:ring focus:outline-none ${className}`;
     } else if (link) {
         cn = `w-max text-${color} hover:underline select-none ${className}`;
     }
@@ -43,14 +43,12 @@ export const Button: React.FC<ButtonProps> = ({
     }
 
     return (
-        <div
-            role="button"
-            tabIndex={0}
+        <button
             className={cn + " flex justify-center relative"}
             onClick={() => execIf(onClick, !isLoading)}
         >
             <div className={isLoading ? "invisible" : ""}>{text}</div>
             {isLoading ? <SemiCircle className="absolute w-6 h-6 animate-spin" /> : null}
-        </div>
+        </button>
     );
 };
