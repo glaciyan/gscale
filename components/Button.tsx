e78b512f26e3e815099956bc9cf0bcc11528ca8e
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-16 16:13:10 +0200

    button loading state

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { SemiCircle } from "./icons/semicircle";
 
 interface ButtonProps {
     text: string;
     color: string;
-    onClick: () => void;
+    onClick?: () => void;
     className?: string;
     secondary?: any;
     ternary?: any;
     link?: any;
     fullw?: any;
+    isLoading?: any;
+}
+
+function execIf(func: any, condition: boolean) {
+    if (condition && func) func();
 }
 
 export const Button: React.FC<ButtonProps> = ({
@@ -20,6 +26,7 @@ export const Button: React.FC<ButtonProps> = ({
     ternary,
     link,
     fullw,
+    isLoading,
 }) => {
     let cn: string = `buttoncommon shadow-md w-max text-gscale-dark-text-primary bg-opacity-70 bg-${color} ${className} transition-colors hover:bg-opacity-50 active:bg-opacity-30 hover:shadow-lg select-none`;
 
@@ -36,8 +43,13 @@ export const Button: React.FC<ButtonProps> = ({
     }
 
     return (
-        <div role="button" tabIndex={0} className={cn} onClick={onClick}>
-            {text}
+        <div
+            role="button"
+            tabIndex={0}
+            className={cn + " flex justify-center"}
+            onClick={() => execIf(onClick, !isLoading)}
+        >
+            {!isLoading ? text : <SemiCircle className="w-6 h-6 animate-spin" />}
         </div>
     );
 };
