b8ab63971e3145fc7af1757c0bc80601c737fb61
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-18 19:35:04 +0200

    info at builds page

--- pages/builds.tsx
+++ pages/builds.tsx
@@ -2,6 +2,7 @@ import buildsDB from "../lib/buildsDatabase";
 import Layout from "../components/Layout";
 import CharacterBuild from "../components/CharacterBuild";
 import { useLiveQuery } from "dexie-react-hooks";
+import { PageDialouge } from "../components/PageDialouge";
 
 export default function Builds() {
     const allBuilds = useLiveQuery(() => buildsDB.builds.toArray(), []);
@@ -9,6 +10,7 @@ export default function Builds() {
     return (
         <Layout title="Builds" current="Your Builds">
             <div className="max-w-screen-xl mx-auto">
+                <PageDialouge text="This page is still unfinished" />
                 {!allBuilds ? (
                     <p>Loading</p>
                 ) : allBuilds.length > 0 ? (
@@ -17,7 +19,10 @@ export default function Builds() {
                             <CharacterBuild
                                 build={build}
                                 key={build.characterId + build.id}
-                                className={(index % 2 === 0 ? "bg-gray-800" : "bg-gray-700") + " p-4"}
+                                className={
+                                    (index % 2 === 0 ? "bg-gray-800" : "bg-gray-700") +
+                                    " p-4"
+                                }
                             />
                         );
                     })
