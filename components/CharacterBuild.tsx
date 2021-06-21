36d5e28916c95c12b7933f277b0da504a0b8b042
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-21 12:45:00 +0200

    edit button

--- components/CharacterBuild.tsx
+++ components/CharacterBuild.tsx
@@ -6,7 +6,8 @@ import { LevelShowcase } from "./LevelShowcase";
 import { SparklesIcon, TrendingUpIcon, FireIcon } from "@heroicons/react/outline";
 import { SwordIcon } from "./icons/sword";
 import { Button } from "./Button";
-import { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction } from "react";
+import Link from "next/link";
 
 export default function CharacterBuild({
     build,
@@ -32,12 +33,16 @@ export default function CharacterBuild({
             <div className="flex">
                 <div className="flex flex-col items-center m-4 mr-8">
                     <ItemCharacterCard className="" character={character} />
-                    <Button
-                        secondary
-                        color={`genshin-element-${character.element}`}
-                        text="Edit"
-                        className="mt-5"
-                    />
+                    <Link href={`/build/${character.id}?edit=${build.id}`}>
+                        <a>
+                            <Button
+                                secondary
+                                color={`genshin-element-${character.element}`}
+                                text="Edit"
+                                className="mt-5"
+                            />
+                        </a>
+                    </Link>
 
                     <Button
                         link
@@ -46,20 +51,6 @@ export default function CharacterBuild({
                         className="mt-2"
                         onClick={() => setDeletingBuild(build)}
                     />
-
-                    {/* <button
-                        className="font-medium bg-red-500 text-gscale-dark-text-primary hover:bg-opacity-90 btn hover:underline"
-                        onClick={async () => {
-                            await buildsDB.builds.delete(build.id);
-                        }}
-                    >
-                        Delete
-                    </button>
-                    <Link href={`/build/${character.id}?edit=${build.id}`}>
-                        <a className="block !py-1.5 bg-blue-500 text-gscale-dark-text-primary font-medium hover:bg-opacity-90 btn hover:underline">
-                            edit
-                        </a>
-                    </Link> */}
                 </div>
                 <div className="flex-grow mt-4">
                     <h1 className="text-lg font-bold">{character.name}</h1>
@@ -99,7 +90,7 @@ export default function CharacterBuild({
             </div>
 
             <div>
-                <h2 className="mt-3 font-bold">Materials needed:</h2>
+                <h2 className="mt-3 ml-5">Materials</h2>
                 <CompletionItemGrid build={build} items={materials.everything} />
             </div>
         </div>
