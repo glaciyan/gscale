e4818f1397d1420b0c7a9d47f63818b322251f4d
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-21 14:15:51 +0200

    prediction

--- lib/buildsDatabase.ts
+++ lib/buildsDatabase.ts
@@ -2,6 +2,7 @@ import dexie from "dexie";
 
 const buildsDB = new dexie("buildsDB");
 
+// completed is going to cause some problems with async later on
 buildsDB.version(1).stores({
     builds: "++id, &order, completed, type, characterId, level, normal, elemental, burst",
 });
