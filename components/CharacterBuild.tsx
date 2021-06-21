8753ded5a5b38249845b5ddb1d0017414734b679
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-21 13:44:49 +0200

    v1 builds layout

--- components/CharacterBuild.tsx
+++ components/CharacterBuild.tsx
@@ -29,69 +29,69 @@ export default function CharacterBuild({
     );
 
     return (
-        <div className="rounded-md bg-gscale-dark-background-secondary bg-opacity-60">
-            <div className="flex">
-                <div className="flex flex-col items-center m-4 mr-8">
-                    <ItemCharacterCard className="" character={character} />
-                    <Link href={`/build/${character.id}?edit=${build.id}`}>
-                        <a>
-                            <Button
-                                secondary
-                                color={`genshin-element-${character.element}`}
-                                text="Edit"
-                                className="mt-5"
-                            />
-                        </a>
-                    </Link>
-
-                    <Button
-                        link
-                        color={"gscale-dark-text-ternary"}
-                        text="Delete"
-                        className="mt-2"
-                        onClick={() => setDeletingBuild(build)}
-                    />
+        <div className="flex flex-col rounded-md bg-gscale-dark-background-secondary bg-opacity-60">
+            <div className="sm:flex">
+                <div className="flex p-4">
+                    <div className="flex flex-col items-center mr-4">
+                        <ItemCharacterCard className="" character={character} />
+                        <Link href={`/build/${character.id}?edit=${build.id}`}>
+                            <a>
+                                <Button
+                                    secondary
+                                    color={`genshin-element-${character.element}`}
+                                    text="Edit"
+                                    className="mt-5"
+                                />
+                            </a>
+                        </Link>
+                        <Button
+                            link
+                            color={"gscale-dark-text-ternary"}
+                            text="Delete"
+                            className="mt-2"
+                            onClick={() => setDeletingBuild(build)}
+                        />
+                    </div>
+                    <div className="mx-4 mb-2">
+                        <h1 className="mb-2 text-lg font-bold">{character.name}</h1>
+                        <LevelShowcase
+                            className=""
+                            left={build.level.start}
+                            right={build.level.goal}
+                            icon={<TrendingUpIcon className="iconsm" />}
+                            label={"Level"}
+                            element={character.element}
+                        ></LevelShowcase>
+                        <LevelShowcase
+                            className="mt-2"
+                            left={build.normal.start}
+                            right={build.normal.goal}
+                            icon={<SwordIcon className="iconsm" />}
+                            label={"Normal"}
+                            element={character.element}
+                        ></LevelShowcase>
+                        <LevelShowcase
+                            className="mt-2"
+                            left={build.elemental.start}
+                            right={build.elemental.goal}
+                            icon={<SparklesIcon className="iconsm" />}
+                            label={"Elemental"}
+                            element={character.element}
+                        ></LevelShowcase>
+                        <LevelShowcase
+                            className="mt-2"
+                            left={build.burst.start}
+                            right={build.burst.goal}
+                            icon={<FireIcon className="iconsm" />}
+                            label={"Burst"}
+                            element={character.element}
+                        ></LevelShowcase>
+                    </div>
                 </div>
-                <div className="flex-grow mt-4">
-                    <h1 className="text-lg font-bold">{character.name}</h1>
-                    <LevelShowcase
-                        className="mt-3"
-                        left={build.level.start}
-                        right={build.level.goal}
-                        icon={<TrendingUpIcon className="iconsm" />}
-                        label={"Level"}
-                        element={character.element}
-                    ></LevelShowcase>
-                    <LevelShowcase
-                        className="mt-2"
-                        left={build.normal.start}
-                        right={build.normal.goal}
-                        icon={<SwordIcon className="iconsm" />}
-                        label={"Normal"}
-                        element={character.element}
-                    ></LevelShowcase>
-                    <LevelShowcase
-                        className="mt-2"
-                        left={build.elemental.start}
-                        right={build.elemental.goal}
-                        icon={<SparklesIcon className="iconsm" />}
-                        label={"Elemental"}
-                        element={character.element}
-                    ></LevelShowcase>
-                    <LevelShowcase
-                        className="mt-2"
-                        left={build.burst.start}
-                        right={build.burst.goal}
-                        icon={<FireIcon className="iconsm" />}
-                        label={"Burst"}
-                        element={character.element}
-                    ></LevelShowcase>
+                <div className="px-5 pt-2 pb-5 rounded-md bg-gscale-dark-background-primary bg-opacity-30">
+                    <h2 className="mb-3 ">Materials</h2>
+                    <CompletionItemGrid build={build} items={materials.everything} />
                 </div>
-            </div>
-
-            <div>
-                <h2 className="mt-3 ml-5">Materials</h2>
-                <CompletionItemGrid build={build} items={materials.everything} />
             </div>
         </div>
     );
