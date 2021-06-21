3a89f7eef8ecc986cb39e4e60ddf7e0ad7b36921
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-21 15:30:37 +0200

    ask to add another character and better no builds

--- pages/builds.tsx
+++ pages/builds.tsx
@@ -5,6 +5,10 @@ import { useLiveQuery } from "dexie-react-hooks";
 import { If } from "../components/If";
 import { ConfirmDeleteDialouge } from "../components/ConfirmDeleteDialouge";
 import { useState } from "react";
+import Link from "next/link";
+import { PlusIcon, XIcon } from "@heroicons/react/outline";
+import { NothingInfo } from "../components/NothingInfo";
+import { Button } from "../components/Button";
 
 export default function Builds() {
     const allBuilds = useLiveQuery(() => buildsDB.builds.toArray(), []);
@@ -16,11 +20,27 @@ export default function Builds() {
             <ConfirmDeleteDialouge build={deletingBuild} setBuild={setDeletingBuild} />
             <Layout title="Builds" current="Your Builds">
                 <div className="mx-3 md:mx-6 2xl:mx-auto max-w-screen-2xl">
-                    <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
-                        {!allBuilds ? (
-                            <p>Loading</p>
-                        ) : (
-                            <If cif={allBuilds.length > 0} celse={<p>No builds</p>}>
+                    {!allBuilds ? (
+                        <p>Loading</p>
+                    ) : (
+                        <If
+                            cif={allBuilds.length > 0}
+                            celse={
+                                <Link href="/">
+                                    <a className="flex flex-col items-center justify-center transition-colors rounded-md">
+                                        <div className="flex flex-col items-center p-6 rounded-md bg-gscale-dark-background-secondary">
+                                            <NothingInfo label="No builds" />
+                                            <Button
+                                                secondary
+                                                className="mt-6 text-blue-100 ring-blue-100"
+                                                text="Back to Characters"
+                                            />
+                                        </div>
+                                    </a>
+                                </Link>
+                            }
+                        >
+                            <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
                                 {allBuilds.map((build: any) => {
                                     return (
                                         <CharacterBuild
@@ -30,9 +50,17 @@ export default function Builds() {
                                         />
                                     );
                                 })}
-                            </If>
-                        )}
-                    </div>
+                                <Link href="/">
+                                    <a className="flex flex-col items-center justify-center py-24 transition-colors rounded-md shadow-md bg-gscale-dark-background-secondary bg-opacity-40 hover:bg-opacity-80">
+                                        <PlusIcon className="w-6 h-6 text-gscale-dark-text-ternary" />
+                                        <div className="text-gscale-dark-text-secondary">
+                                            Add another character
+                                        </div>
+                                    </a>
+                                </Link>
+                            </div>
+                        </If>
+                    )}
                 </div>
             </Layout>
         </>
