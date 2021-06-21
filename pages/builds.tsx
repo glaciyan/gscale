7b6900e86bcde65053122472e0e362d92c2ca208
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-21 15:41:06 +0200

    cleaned up no builds and removed reccomendation

--- pages/builds.tsx
+++ pages/builds.tsx
@@ -5,10 +5,7 @@ import { useLiveQuery } from "dexie-react-hooks";
 import { If } from "../components/If";
 import { ConfirmDeleteDialouge } from "../components/ConfirmDeleteDialouge";
 import { useState } from "react";
-import Link from "next/link";
-import { PlusIcon } from "@heroicons/react/outline";
 import { NothingInfo } from "../components/NothingInfo";
-import { Button } from "../components/Button";
 
 export default function Builds() {
     const allBuilds = useLiveQuery(() => buildsDB.builds.toArray(), []);
@@ -25,20 +22,7 @@ export default function Builds() {
                     ) : (
                         <If
                             cif={allBuilds.length > 0}
-                            celse={
-                                <Link href="/">
-                                    <a className="flex flex-col items-center justify-center transition-colors rounded-md">
-                                        <div className="flex flex-col items-center p-6 rounded-md bg-gscale-dark-background-secondary">
-                                            <NothingInfo label="No builds" />
-                                            <Button
-                                                secondary
-                                                className="mt-6 text-blue-100 ring-blue-100"
-                                                text="Back to Characters"
-                                            />
-                                        </div>
-                                    </a>
-                                </Link>
-                            }
+                            celse={<NothingInfo label="No builds" />}
                         >
                             <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
                                 {allBuilds.map((build: any) => {
@@ -50,14 +34,14 @@ export default function Builds() {
                                         />
                                     );
                                 })}
-                                <Link href="/">
+                                {/* <Link href="/">
                                     <a className="flex flex-col items-center justify-center py-24 transition-colors rounded-md shadow-md bg-gscale-dark-background-secondary bg-opacity-40 hover:bg-opacity-80">
                                         <PlusIcon className="w-6 h-6 text-gscale-dark-text-ternary" />
                                         <div className="text-gscale-dark-text-secondary">
                                             Add another character
                                         </div>
                                     </a>
-                                </Link>
+                                </Link> */}
                             </div>
                         </If>
                     )}
