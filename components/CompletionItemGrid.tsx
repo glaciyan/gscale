08f91cf485aa1366f46e24014aae5221ee7c9d9b
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-06-21 12:08:23 +0200

    using new checklistitemcard

--- components/CompletionItemGrid.tsx
+++ components/CompletionItemGrid.tsx
@@ -1,8 +1,12 @@
+import { CheckIcon } from "@heroicons/react/outline";
 import millify from "millify";
 import { BuildItem, Item } from "../data/items";
 import { toId } from "../lib";
 import buildsDB from "../lib/buildsDatabase";
-import ItemCard from "./ItemCard";
+import CheckListItemCard from "./CheckListItemCard";
+import { Transition } from "@headlessui/react";
+import { If } from "./If";
+import { useState } from "react";
 
 export default function CompletionItemGrid({
     items,
@@ -12,7 +16,7 @@ export default function CompletionItemGrid({
     build: any;
 }) {
     return (
-        <div className="flex flex-wrap">
+        <div className="flex flex-wrap mx-4 mb-4">
             {items
                 .sort((a, b) => a.order - b.order)
                 .map((item) => {
@@ -22,54 +26,17 @@ export default function CompletionItemGrid({
                         : false;
 
                     return (
-                        <button
-                            key={toId(item.name)}
-                            onClick={() => {
-                                if (build.completed) {
-                                    const find = build.completed.findIndex(
-                                        (element: Item) => element.name === item.name
-                                    );
-
-                                    if (find > -1) {
-                                        const completed = [...build.completed];
-                                        completed.splice(find, 1);
-
-                                        buildsDB.builds.update(
-                                            Number.parseInt(build.id),
-                                            { completed: completed }
-                                        );
-                                        return;
-                                    }
-                                }
-
-                                buildsDB.builds.update(Number.parseInt(build.id), {
-                                    completed: build.completed
-                                        ? [
-                                              {
-                                                  name: item.name,
-                                                  amount: item.amount,
-                                              },
-                                              ...build.completed,
-                                          ]
-                                        : [
-                                              {
-                                                  name: item.name,
-                                                  amount: item.amount,
-                                              },
-                                          ],
-                                });
-                            }}
-                        >
-                            <ItemCard
-                                completed={done}
-                                item={item}
-                                label={
-                                    item.amount > 999
-                                        ? millify(item.amount)
-                                        : String(item.amount)
-                                }
-                            />
-                        </button>
+                        <CheckListItemCard
+                            key={item.name}
+                            done={done}
+                            build={build}
+                            item={item}
+                            label={
+                                item.amount > 999
+                                    ? millify(item.amount)
+                                    : String(item.amount)
+                            }
+                        />
                     );
                 })}
         </div>
