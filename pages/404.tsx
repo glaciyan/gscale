4757248951e3bea5a3991dbef9433398c7da8fd1
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-07-15 16:37:36 +0200

    adjusted 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -9,9 +9,9 @@ const NotFound: React.FC<NotFoundProps> = ({}) => {
         <Layout title={"Not Found"}>
             <div className={`flex flex-col w-full h-screen mt-24 items-center`}>
                 <div className={`mb-12`}>
-                    <Image src="/images/404.svg" width="1920" height="300"></Image>
+                    <Image src="/images/404.svg" width="600" height="240"></Image>
                 </div>
-                <h1 className={`text-gscale-dark-text-ternary text-6xl`}>404</h1>
+                <h1 className={`text-gscale-dark-text-secondary text-5xl`}>404</h1>
                 <div className={`text-gscale-dark-text-ternary`}>Not Found</div>
             </div>
         </Layout>
