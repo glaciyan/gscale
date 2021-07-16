774c126501c48cc6fe943cc3b3ae06b29b90e52e
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-07-16 09:52:57 +0200

    remove next-optimized-image

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withPlugins(
     [
-        [optimizedImages, {}],
         [withBundleAnalyzer],
 
         // your other plugins here
