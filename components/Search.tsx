da0804d9cf55b7cd0c19f3409aab212afea84634
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-15 10:36:36 +0200

    search styling fixes and placeholder

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useIsTouch } from "./useIsTouch";
 
 interface SearchProps {
     onConfirm: any;
@@ -6,6 +7,8 @@ interface SearchProps {
 }
 
 export const Search: React.FC<SearchProps> = ({ onConfirm, onChange }) => {
+    const isTouch = useIsTouch();
+
     return (
         <form
             action="#"
@@ -13,13 +16,13 @@ export const Search: React.FC<SearchProps> = ({ onConfirm, onChange }) => {
                 onConfirm();
                 e.preventDefault();
             }}
-            className="flex items-center mb-6 overflow-hidden transition-shadow rounded-md shadow-md focus-within:ring-2 ring-gscale-blue-base md:mx-auto bg-gscale-dark-background-primary text-gscale-dark-text-secondary"
+            className="flex items-center mb-6 py-2.5 px-4 overflow-hidden rounded-md shadow-md focus-within:ring-2 focus-within:ring-genshin-element-cryo transition-shadow md:mx-auto bg-gscale-dark-background-primary text-gscale-dark-text-secondary"
             style={{ alignItems: "stretch" }}
         >
             <button
                 type="submit"
                 aria-label="Search"
-                className="flex items-center px-4 hover:bg-opacity-30 text-gscale-dark-text-ternary focus:outline-none active:text-gscale-dark-text-primary hover:text-gscale-dark-text-secondary"
+                className="flex items-center mr-4 hover:bg-opacity-30 text-gscale-dark-text-ternary focus:outline-none active:text-gscale-dark-text-primary hover:text-gscale-dark-text-secondary"
             >
                 <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path
@@ -33,11 +36,15 @@ export const Search: React.FC<SearchProps> = ({ onConfirm, onChange }) => {
             </button>
             <input
                 type="text"
-                placeholder={"Search for anything"}
+                placeholder={
+                    isTouch
+                        ? "Search for anything"
+                        : "Search for Names, Materials, Elements, etc..."
+                }
                 autoCorrect="false"
                 spellCheck="false"
                 autoCapitalize="false"
-                className="flex-1 py-2.5 focus:outline-none bg-gscale-dark-background-primary placeholder-gscale-dark-text-ternary"
+                className="flex-1 focus:outline-none bg-gscale-dark-background-primary placeholder-gscale-dark-text-ternary overflow-ellipsis"
                 onChange={onChange}
                 // onFocus={(e) => {
                 //     const searchBar = e.target as HTMLInputElement;
