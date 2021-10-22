2151db46ff9adef2f161d343caf5c88eab11167a
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-22 17:54:00 +0200

    header and footer

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,16 @@ import { RouterView, RouterLink } from "vue-router";
 </script>
 
 <template>
-    <header>
-        <span>header</span>
+    <header class="flex space-x-2">
+        <span>gscale</span>
         <RouterLink to="/">Characters</RouterLink>
         <RouterLink to="/builds">Builds</RouterLink>
     </header>
-    <main>
+    <main class="h-screen">
         <RouterView></RouterView>
     </main>
+    <footer>
+        <hr />
+        <span>footer</span>
+    </footer>
 </template>
