939b6638193afebdd5a06834eca0c1e4319b0ded
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-23 11:47:49 +0200

    refactor character class

--- src/data/Characters.ts
+++ src/data/Characters.ts
@@ -1,21 +1,4 @@
-import { toId } from "../lib/toId";
-import { ICharacter } from "./contracts/ICharacter";
-
-export class GICharacter implements ICharacter {
-    name: string;
-    weaponType: string;
-    element: string;
-    rarity: 4 | 5;
-    id: string;
-
-    constructor(name: string, weaponType: string, element: string, rarity: 4 | 5) {
-        this.id = toId(name);
-        this.name = name;
-        this.weaponType = weaponType;
-        this.element = element;
-        this.rarity = rarity;
-    }
-}
+import { GICharacter } from "./entities/GICharacter";
 
 export const characters: GICharacter[] = [
     new GICharacter("Amber", "bow", "pyro", 4),
