c3e1b2a930d4cf47e422264815d00541cad4ec6a
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-25 14:43:45 +0200

    image intersection loading

--- src/components/character/CharacterCard.vue
+++ src/components/character/CharacterCard.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import RarityStars from "../RarityStars.vue";
 import { RouterLink } from "vue-router";
+import { onMounted } from "@vue/runtime-core";
+import { Ref, ref } from "vue";
+import { useIntersection } from "../../composites/useIntersection";
 
 const props = defineProps<{
     name: string;
@@ -15,6 +18,10 @@ const props = defineProps<{
 
 const elementTextColor = `text-genshin-element-${props.elementId}`;
 const hoverColor = `ring-genshin-element-${props.elementId}`;
+
+const card = ref(null);
+
+const intersecting = useIntersection(card);
 </script>
 
 <template>
@@ -24,8 +31,17 @@ const hoverColor = `ring-genshin-element-${props.elementId}`;
             hoverColor,
         ]"
     >
-        <div class="bg-gray-700">
-            <img :src="cardUrl" :alt="`Image of ${name}`" width="240" height="150" />
+        <div class="bg-gray-700 h-[150px] w-[240px]" ref="card">
+            <img
+                v-if="intersecting"
+                :src="cardUrl"
+                :alt="`Image of ${name}`"
+                decoding="async"
+                loading="lazy"
+                draggable="false"
+                width="240"
+                height="150"
+            />
         </div>
         <div class="bg-gray-600 py-3 px-4">
             <div class="flex flex-wrap space-x-1 items-center">
---
+++ src/composites/useIntersection.ts
@@ -0,0 +1,25 @@
+import { onMounted, Ref, ref } from "vue";
+
+export function useIntersection(
+    element: Ref<any>,
+    threshold = 0.1,
+    rootMargin = "350px"
+) {
+    const intersecting = ref(false);
+
+    const handleIntersect = (entries: IntersectionObserverEntry[]) => {
+        if (entries[0].intersectionRatio > 0) {
+            intersecting.value = true;
+        }
+    };
+
+    onMounted(() => {
+        const observer = new IntersectionObserver(handleIntersect, {
+            threshold,
+            rootMargin,
+        });
+        observer.observe(element.value);
+    });
+
+    return intersecting;
+}
