886aca3f0ebba5674f071a7ab3410bdc49f50191
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-29 11:24:04 +0200

    auto import

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import windi from "vite-plugin-windicss";
 import svg from "vite-svg-loader";
+import autoImport from "unplugin-auto-import/vite";
+import components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +13,13 @@ export default defineConfig({
             "~/": `${path.resolve(__dirname, "src")}/`,
         },
     },
-    plugins: [vue(), windi(), svg()],
+    plugins: [
+        vue(),
+        windi(),
+        svg(),
+        autoImport({
+            imports: ["vue", "vue-router", "@vueuse/core"],
+        }),
+        components(),
+    ],
 });
