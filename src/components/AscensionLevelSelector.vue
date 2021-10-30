36333f16a2152f5ef0810cb1d17ca2336f788268
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-30 10:55:50 +0200

    ascension star

--- src/components/AscensionLevelSelector.vue
+++ src/components/AscensionLevelSelector.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from "@headlessui/vue";
 import { AscensionLevel, Levels } from "~/interfaces/AscensionLevel";
+import AscensionStar from "~/assets/icons/asc_star.svg";
 
 defineProps<{ value: AscensionLevel }>();
 </script>
 
 <template>
     <Listbox :model-value="value" @update:model-value="$emit('update:value', $event)">
-        <ListboxButton>{{ value.level }} {{ value.ascended }}</ListboxButton>
+        <ListboxButton class="">{{ value.level }} {{ value.ascended }}</ListboxButton>
         <ListboxOptions>
-            <ListboxOption v-for="option in Levels" :key="option.level + option.ascended.toString()" :value="option">
-                {{ option.level }} {{ option.ascended }}
+            <ListboxOption
+                class="flex items-center"
+                v-for="option in Levels"
+                :key="option.level + option.ascended.toString()"
+                :value="option"
+            >
+                {{ option.level }} <AscensionStar v-if="option.ascended" class="ml-1" />
             </ListboxOption>
         </ListboxOptions>
     </Listbox>
