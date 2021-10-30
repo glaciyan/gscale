cebb26db094d7dc99dcd24a4e7e030b8fdfef6a5
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-30 11:11:16 +0200

    patch vite to correctly import svg

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,10 @@ declare module "*.vue" {
     const component: DefineComponent<{}, {}, any>;
     export default component;
 }
+
+declare module "*.svg" {
+    import { DefineComponent } from "vue";
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+    const svg: DefineComponent<{}, {}, any>;
+    export default svg;
+}
