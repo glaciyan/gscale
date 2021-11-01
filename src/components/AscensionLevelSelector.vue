b26764e95f5b7c8969d67f64c38fb4a16fb6e8c4
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-11-01 11:10:19 +0100

    style options

--- src/components/AscensionLevelSelector.vue
+++ src/components/AscensionLevelSelector.vue
@@ -40,39 +40,59 @@ export default defineComponent({
 
 <template>
     <Listbox :modelValue="value" @update:modelValue="$emit('update:value', $event)">
-        <div class="flex">
-            <ListboxButton
-                class="
-                    cursor-default
-                    font-normal font-sans
-                    bg-dark-400
-                    border-r-[1px] border-dark-500
-                    text-base text-left
-                    w-max
-                    py-2
-                    px-4
-                    text-light-900
-                "
-            >
-                <div>{{ value.level }}</div>
-            </ListboxButton>
-            <AscensionCheckbox
-                :checkboxId="checkboxId"
-                :modelValue="value.ascended"
-                :disabled="cannotAscend"
-                @update:modelValue="update({ ascended: $event })"
-            />
+        <div class="relative">
+            <div class="flex">
+                <ListboxButton
+                    class="
+                        cursor-default
+                        font-normal font-sans
+                        bg-dark-400
+                        border-r-[1px] border-dark-500
+                        text-base text-left
+                        w-max
+                        py-2
+                        px-4
+                        text-light-900
+                    "
+                >
+                    <div>{{ value.level }}</div>
+                </ListboxButton>
+                <AscensionCheckbox
+                    :checkboxId="checkboxId"
+                    :modelValue="value.ascended"
+                    :disabled="cannotAscend"
+                    @update:modelValue="update({ ascended: $event })"
+                />
+            </div>
+            <transition name="appear">
+                <ListboxOptions
+                    class="rounded-md list-none bg-dark-400 m-0 mt-1 w-max max-h-96 p-0 py-1 overflow-y-auto"
+                >
+                    <ListboxOption
+                        v-slot="{ active }"
+                        class=""
+                        v-for="option in Levels"
+                        :key="option.level + option.ascended.toString()"
+                        :value="option"
+                    >
+                        <div :class="['flex items-center cursor-default py-2 pl-6 pr-6', { 'bg-dark-700': active }]">
+                            {{ option.level }} <AscensionStarIcon v-if="option.ascended" class="flex-shrink-0 ml-1" />
+                        </div>
+                    </ListboxOption>
+                </ListboxOptions>
+            </transition>
         </div>
-        <ListboxOptions>
-            <ListboxOption
-                v-slot="{ active }"
-                class="flex items-center"
-                v-for="option in Levels"
-                :key="option.level + option.ascended.toString()"
-                :value="option"
-            >
-                {{ active }} {{ option.level }} <AscensionStarIcon v-if="option.ascended" class="ml-1" />
-            </ListboxOption>
-        </ListboxOptions>
     </Listbox>
 </template>
+
+<style scoped>
+.appear-enter-active,
+.appear-leave-active {
+    @apply transition-opacity duration-100;
+}
+
+.appear-enter-from,
+.appear-leave-to {
+    @apply opacity-0;
+}
+</style>
