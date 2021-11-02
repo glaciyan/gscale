e4ec4bdd66cdcb516a0300fc42fc62a35af053f4
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-11-02 14:55:32 +0100

    rounded stroke width

--- src/components/icons/SwordIcon.vue
+++ src/components/icons/SwordIcon.vue
@@ -3,7 +3,7 @@
         <path
             d="M9.05827 14.9414L5.69652 18.3031M7.39168 13.2748L14.9699 5.69656L18.3031 5.69656L18.3031 9.02974L10.7249 16.608L7.39168 13.2748ZM5.69652 11.5796L12.42 18.3031L5.69652 11.5796Z"
             stroke="currentColor"
-            stroke-width="1.49759"
+            stroke-width="1.5"
             stroke-miterlimit="1.5"
             stroke-linecap="round"
             stroke-linejoin="round"
