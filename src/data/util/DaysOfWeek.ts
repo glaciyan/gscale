528b289c239f1d93e2935fed8a6af3a53c777466
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-11-04 20:50:27 +0100

    days of week

---
+++ src/data/util/DaysOfWeek.ts
@@ -0,0 +1,30 @@
+export const DaysOfWeek = {
+    monday: {
+        name: "Monday",
+        normalizedName: "mo",
+    },
+    tuesday: {
+        name: "Thuesday",
+        normalizedName: "tu",
+    },
+    wednesday: {
+        name: "Wednesday",
+        normalizedName: "we",
+    },
+    thursday: {
+        name: "Thursday",
+        normalizedName: "th",
+    },
+    friday: {
+        name: "Friday",
+        normalizedName: "fr",
+    },
+    saturday: {
+        name: "Saturday",
+        normalizedName: "sa",
+    },
+    sunday: {
+        name: "Sunday",
+        normalizedName: "su",
+    },
+};
