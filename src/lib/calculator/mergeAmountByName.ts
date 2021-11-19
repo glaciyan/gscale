25b6e94ce286158fea168ad4931d92eea5edb542
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-11-19 13:21:45 +0100

    merge reduce function

---
+++ src/lib/calculator/mergeAmountByName.ts
@@ -0,0 +1,18 @@
+import { ItemWithAmount } from "../data/entities/ItemWithAmount";
+
+export default (total: ItemWithAmount[], current: ItemWithAmount[]) => {
+  current.forEach((item) => {
+    if (item) {
+      const itemWithSameNameIndex = total.findIndex((i) => i?.item.normalizedName === item.item.normalizedName);
+
+      if (itemWithSameNameIndex === -1) {
+        total.push(item);
+      } else {
+        //@ts-ignore will not be undefined
+        total[itemWithSameNameIndex].amount += item.amount;
+      }
+    }
+  });
+
+  return total;
+};
