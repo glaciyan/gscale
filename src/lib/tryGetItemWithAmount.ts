47b3332b117f0e025d12abbba48c4ff0ad56d3ee
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-11-19 13:20:35 +0100

    remove redundant null

--- src/lib/tryGetItemWithAmount.ts
+++ src/lib/tryGetItemWithAmount.ts
@@ -4,7 +4,7 @@ import { ItemWithAmount } from "./data/entities/ItemWithAmount";
 /**
  * Constructs an ItemWithAmount or null if any parameters are falsy
  */
-export function tryGetItemWithAmount(item?: IItem, amount?: number): ItemWithAmount | null {
+export function tryGetItemWithAmount(item?: IItem, amount?: number): ItemWithAmount {
   if (item && amount) {
     return { item, amount };
   } else {
