53b7fd76a97661b7c289fef4b39e22ca39bd177d
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-11-20 13:38:11 +0100

    leveling calculator

--- src/lib/calculator/index.ts
+++ src/lib/calculator/index.ts
@@ -2,18 +2,43 @@ import AscensionCostTable from "../data/AscensionCostTable";
 import { IBaseCharacter } from "../data/contracts/IBaseCharacter";
 import { ICharacter } from "../data/contracts/ICharacter";
 import { ITraveler } from "../data/contracts/ITraveler";
-import { IItemWithAmountNotNull, ItemWithAmount } from "../data/entities/ItemWithAmount";
+import { IItemWithAmountNotNull } from "../data/entities/ItemWithAmount";
+import { Items } from "../data/Items";
 import LevelingCostTable from "../data/LevelingCostTable";
 import { getAscensionStage } from "../getAscensionStage";
 import getLevelingIndex from "../getLevelingIndex";
 import { AscensionLevel } from "../interfaces/AscensionLevel";
 import mergeAmountByName from "../mergeAmountByName";
 
-export function calculateLeveling(start: AscensionLevel, goal: AscensionLevel) {
-  const startAscension = getLevelingIndex(start);
-  const goalAscension = getLevelingIndex(goal);
+export function calculateLeveling(
+  start: AscensionLevel,
+  goal: AscensionLevel
+): { mora: IItemWithAmountNotNull; lazy: IItemWithAmountNotNull; accurate: IItemWithAmountNotNull[] } {
+  const startLevelIndex = getLevelingIndex(start);
+  const goatLevelIndex = getLevelingIndex(goal);
 
-  return LevelingCostTable.slice(startAscension, goalAscension);
+  const cost = LevelingCostTable.slice(startLevelIndex, goatLevelIndex).reduce(
+    (prev, current) => {
+      prev.xp += current.xp;
+      prev.mora += current.mora;
+      prev.accurate[0] += current.accurate[0];
+      prev.accurate[1] += current.accurate[1];
+      prev.accurate[2] += current.accurate[2];
+
+      return prev;
+    },
+    { xp: 0, mora: 0, accurate: [0, 0, 0] }
+  );
+
+  return {
+    mora: { item: Items.mora, amount: cost.mora },
+    lazy: { item: Items.heros_wit, amount: Math.ceil(cost.xp / 20000) },
+    accurate: [
+      { item: Items.heros_wit, amount: cost.accurate[0] },
+      { item: Items.adventurers_experience, amount: cost.accurate[1] },
+      { item: Items.wanderers_advice, amount: cost.accurate[2] },
+    ],
+  };
 }
 
 export function calculateAscension(
