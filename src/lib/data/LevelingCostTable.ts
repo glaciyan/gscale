ad085dbe01240ecc0664920cea90aa306a93b596
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-11-20 12:41:19 +0100

    remove leveling prop

--- src/lib/data/LevelingCostTable.ts
+++ src/lib/data/LevelingCostTable.ts
@@ -1,9 +1,9 @@
 export default [
-  { level: 20, xp: 120175, mora: 24200, accurate: [6, 0, 1] },
-  { level: 40, xp: 578325, mora: 115800, accurate: [28, 3, 4] },
-  { level: 50, xp: 579100, mora: 116000, accurate: [29, 0, 0] },
-  { level: 60, xp: 854125, mora: 171000, accurate: [42, 3, 0] },
-  { level: 70, xp: 1195925, mora: 239200, accurate: [59, 3, 1] },
-  { level: 80, xp: 1611875, mora: 322400, accurate: [80, 2, 2] },
-  { level: 90, xp: 3423125, mora: 684800, accurate: [171, 0, 4] },
+  { xp: 120175, mora: 24200, accurate: [6, 0, 1] }, // from 0 to 20
+  { xp: 578325, mora: 115800, accurate: [28, 3, 4] },
+  { xp: 579100, mora: 116000, accurate: [29, 0, 0] },
+  { xp: 854125, mora: 171000, accurate: [42, 3, 0] },
+  { xp: 1195925, mora: 239200, accurate: [59, 3, 1] },
+  { xp: 1611875, mora: 322400, accurate: [80, 2, 2] },
+  { xp: 3423125, mora: 684800, accurate: [171, 0, 4] }, // from 80 to 90
 ];
