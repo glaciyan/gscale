b6375add81d613afa2bc540dcdd1bb741e944995
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-11-20 11:09:33 +0100

    filter items from merge with amount of 0

--- src/lib/mergeAmountByName.ts
+++ src/lib/mergeAmountByName.ts
@@ -2,7 +2,7 @@ import { ItemWithAmount } from "./data/entities/ItemWithAmount";
 
 export default (total: ItemWithAmount[], current: ItemWithAmount[]) => {
   current.forEach((item) => {
-    if (item) {
+    if (item && item.amount > 0) {
       const itemWithSameNameIndex = total.findIndex((i) => i?.item.normalizedName === item.item.normalizedName);
 
       if (itemWithSameNameIndex === -1) {
