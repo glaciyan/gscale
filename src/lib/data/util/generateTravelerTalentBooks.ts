2281d0a5f3b144782bc6c5692f2fa131d82826b3
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-11-22 10:46:30 +0100

    fix traveler talent book indexes

--- src/lib/data/util/generateTravelerTalentBooks.ts
+++ src/lib/data/util/generateTravelerTalentBooks.ts
@@ -7,14 +7,14 @@ export function generateTravelerTalentBooks(
   third: IItemGroup
 ): ITravelerTalentBooks {
   return {
-    2: first,
-    3: second,
-    4: third,
-    5: first,
-    6: second,
-    7: third,
-    8: first,
-    9: second,
-    10: third,
+    1: first,
+    2: second,
+    3: third,
+    4: first,
+    5: second,
+    6: third,
+    7: first,
+    8: second,
+    9: third,
   };
 }
