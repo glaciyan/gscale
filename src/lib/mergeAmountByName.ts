8d72cf493b7d9dfa72633676560db03731adfa29
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-11-24 12:25:42 +0100

    helper in merge amount by name

--- src/lib/mergeAmountByName.ts
+++ src/lib/mergeAmountByName.ts
@@ -7,14 +7,22 @@ import { IItemWithAmountNotNull, ItemWithAmount } from "./data/entities/ItemWith
  */
 const merge = (total: ItemWithAmount[], current: ItemWithAmount[]) => {
   current.forEach((item) => {
-    if (item && item.amount > 0) {
-      const itemWithSameNameIndex = total.findIndex((i) => i?.item.normalizedName === item.item.normalizedName);
+    if (item) {
+      if (item.amount > 0) {
+        const itemWithSameNameIndex = total.findIndex((i) => i?.item.normalizedName === item.item.normalizedName);
 
-      if (itemWithSameNameIndex === -1) {
-        total.push(Object.assign({}, item));
-      } else {
-        //@ts-ignore will not be undefined
-        total[itemWithSameNameIndex].amount += item.amount;
+        if (itemWithSameNameIndex === -1) {
+          total.push(Object.assign({}, item));
+        } else {
+          //@ts-ignore will not be undefined
+          total[itemWithSameNameIndex].amount += item.amount;
+        }
+      }
+    } else {
+      // undefined is used for cases like the traveler who doesn't have a boss item for ascension
+      // null case is when there really was an error getting the item see tryGetItemWithAmount.ts and itemFunc.ts
+      if (item === null) {
+        console.error(`An item is null. Item won't show up. This might be happening because of a typo.`);
       }
     }
   });
