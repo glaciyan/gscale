f24914f95d98c36c73359a8124ef61d11a9ca80d
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-07 20:57:38 +0100

    new cooler spinner

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,17 +1,81 @@
 <template>
-  <svg
-    class="animate-spin animate-ease-in-out animate-duration-1000 inline-block"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+  <svg class="inline-block" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-      d="M18 15.889C17.22 17.0401 16.1115 17.9373 14.8145 18.4672C13.5174 18.997 12.0903 19.1356 10.7134 18.8655C9.33645 18.5954 8.07169 17.9287 7.079 16.9497C6.0863 15.9708 5.41027 14.7235 5.13639 13.3656C4.86251 12.0078 5.00308 10.6003 5.54032 9.32121C6.07756 8.04213 6.98734 6.94888 8.15462 6.17971C9.3219 5.41054 10.6943 5 12.0981 5"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
+      class="ani-pulse-1"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M16.2387 8.12492L22 11H13.4114L13 10.6339V3L16.238 8.12492H16.2387Z"
+      fill="currentColor"
+    />
+    <path
+      class="ani-pulse-2"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M14 13L16.8678 16L22 13H14V13Z"
+      fill="currentColor"
+    />
+    <path
+      class="ani-pulse-3"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M13 15V23L16 17.8684L13 15.0006V15Z"
+      fill="currentColor"
+    />
+    <path
+      class="ani-pulse-4"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M11 14L8 16.868L11 22V14.0012V14Z"
+      fill="currentColor"
+    />
+    <path
+      class="ani-pulse-5"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M6.96191 10L2 12.2856V12.7144L6.96191 15L10 12.5014L6.96191 10V10Z"
+      fill="currentColor"
+    />
+    <path
+      class="ani-pulse-6"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M8 8.13203L11 11V3L8 8.13203Z"
+      fill="currentColor"
     />
   </svg>
 </template>
+
+<style scoped lang="less">
+@keyframes pulse {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0.1;
+  }
+}
+
+@-webkit-keyframes pulse {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0.1;
+  }
+}
+
+.animate(@animation) {
+  -webkit-animation: @animation;
+  animation: @animation;
+}
+
+.generate-ani-pulse(6);
+
+.generate-ani-pulse(@count, @i: 1) when (@i <= @count) {
+  .ani-pulse-@{i} {
+    .animate(pulse 0.4s ease 0.05s * @i infinite alternate);
+  }
+
+  .generate-ani-pulse(@count, (@i + 1));
+}
+</style>
