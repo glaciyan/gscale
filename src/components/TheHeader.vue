c106baa0b94a28679e4357caa76292b8a3bfaf87
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-08 19:27:18 +0100

    logo link

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,11 +2,14 @@
 import Navigation from "~/components/TheVerticalNavigation.vue";
 import LogoWithLabel from "~/assets/logo-label.svg?component";
 import Container from "./Container.vue";
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
   <Container as="header" class="flex space-x-2 h-28 items-center">
-    <LogoWithLabel class="mr-16" />
+    <RouterLink to="/" class="mr-16">
+      <LogoWithLabel />
+    </RouterLink>
     <Navigation orientation="verical" />
   </Container>
 </template>
