dd1668bdd3ea7323b96d3b7c02cff778266fc48d
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-08 19:04:36 +0100

    getItem for repo

--- src/lib/data/repository/GenshinDataRepository.ts
+++ src/lib/data/repository/GenshinDataRepository.ts
@@ -1,6 +1,7 @@
 import { getById } from "../util/getById";
 import { Characters } from "../Characters";
 import { Travelers } from "../Travelers";
+import { Items } from "../Items";
 
 export default {
   getCharacter(id: string) {
@@ -8,5 +9,8 @@ export default {
   },
   getTraveler(id: string) {
     return getById(Travelers, id);
+  },
+  getItem(id: string) {
+    return getById(Items, id);
   },
 };
