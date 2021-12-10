35555800b28f5f857e4bee2bb6e2323af04783ec
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-10 10:50:02 +0100

    set height for mugshot

--- src/components/character/CharacterPortrait.vue
+++ src/components/character/CharacterPortrait.vue
@@ -8,7 +8,7 @@ const mugshotRounding = ["rounded-t-md", { "rounded-b-md": props.rarity === unde
 
 <template>
   <div class="rounded-md bg-dark-500 w-[100px]">
-    <div :class="['bg-dark-500 aspect-w-1 aspect-h-1', mugshotRounding]">
+    <div :class="['bg-dark-500 w-[100px] h-[100px]', mugshotRounding]">
       <img :class="mugshotRounding" :src="getCharacterImage(normalizedName, 'mugshot').webp" width="100" height="100" />
     </div>
     <div v-if="rarity" class="rounded-b-md flex bg-dark-400 border-0 py-0.5 items-center justify-center">
