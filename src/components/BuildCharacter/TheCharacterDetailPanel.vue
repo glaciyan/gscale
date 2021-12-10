fd10e68b97ab17b0d41213a3b7733bea495b9524
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-10 10:28:15 +0100

    no z index background image for detail panel

--- src/components/BuildCharacter/TheCharacterDetailPanel.vue
+++ src/components/BuildCharacter/TheCharacterDetailPanel.vue
@@ -13,7 +13,7 @@ const elementTextColor = `text-genshin-element-${props.character.element.normali
 
 <template>
   <aside class="flex flex-col h-full bg-dark-700 px-6 pt-4 pb-6 relative">
-    <div aria-hidden="true" class="h-32 w-full opacity-50 inset-x-0 top-0 z-10 absolute">
+    <div aria-hidden="true" class="h-32 w-full opacity-50 inset-x-0 top-0 absolute">
       <img
         class="h-full object-cover w-full"
         :src="getCharacterImage(character.normalizedName, 'card').webp"
@@ -22,38 +22,40 @@ const elementTextColor = `text-genshin-element-${props.character.element.normali
         draggable="false"
       />
     </div>
-    <CharacterPortrait
-      class="ring mb-6 ring-dark-700 z-20"
-      :normalizedName="character.normalizedName"
-      :rarity="character.rarity"
-    />
-    <!-- Character details -->
-    <div>
-      <p class="font-bold text-light-important">{{ character.name }}</p>
-      <p>{{ character.constellation }}</p>
-      <TheClampedText class="mt-1 text-sm text-light-ternary leading-4 block sm:w-64">
-        {{ character.description }}
-      </TheClampedText>
-    </div>
-    <div class="space-y-1 my-6">
-      <IconWithText>
-        <template #icon>
-          <ElementIcon class="text-dark-100" />
-        </template>
-        <template #default>
-          <p :class="elementTextColor">{{ character.element.name }}</p>
-        </template>
-      </IconWithText>
-      <IconWithText>
-        <template #icon>
-          <SwordIcon class="text-dark-100 self-start" />
-        </template>
-        <template #default>
-          <div class="text-dark-50">
-            <div>{{ character.weaponType.name }}<br />{{ character.sub }}</div>
-          </div>
-        </template>
-      </IconWithText>
+    <div class="relative">
+      <CharacterPortrait
+        class="ring mb-6 ring-dark-700"
+        :normalizedName="character.normalizedName"
+        :rarity="character.rarity"
+      />
+      <!-- Character details -->
+      <div>
+        <p class="font-bold text-light-important">{{ character.name }}</p>
+        <p>{{ character.constellation }}</p>
+        <TheClampedText class="mt-1 text-sm text-light-ternary leading-4 block sm:w-64">
+          {{ character.description }}
+        </TheClampedText>
+      </div>
+      <div class="space-y-1 my-6">
+        <IconWithText>
+          <template #icon>
+            <ElementIcon class="text-dark-100" />
+          </template>
+          <template #default>
+            <p :class="elementTextColor">{{ character.element.name }}</p>
+          </template>
+        </IconWithText>
+        <IconWithText>
+          <template #icon>
+            <SwordIcon class="text-dark-100 self-start" />
+          </template>
+          <template #default>
+            <div class="text-dark-50">
+              <div>{{ character.weaponType.name }}<br />{{ character.sub }}</div>
+            </div>
+          </template>
+        </IconWithText>
+      </div>
     </div>
   </aside>
 </template>
