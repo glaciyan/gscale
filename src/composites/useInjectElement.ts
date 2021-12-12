7c93c0e8d98551538a77e371aeb28f28c7c3b0a2
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-12 17:20:00 +0100

    better default for inject element

--- src/composites/useInjectElement.ts
+++ src/composites/useInjectElement.ts
@@ -1,5 +1,6 @@
 import { IElement } from "~/lib/data/contracts/IElement";
+import { Elements } from "~/lib/data/Elements";
 
 export function useInjectElement() {
-  return inject("element", undefined) as IElement | undefined;
+  return inject("element", Elements.neutral) as IElement;
 }
