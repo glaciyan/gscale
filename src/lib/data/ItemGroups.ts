f3948193d6218eed95fda31c8abd88e928cc5973
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-13 11:29:33 +0100

    normalized name for items groups

--- src/lib/data/ItemGroups.ts
+++ src/lib/data/ItemGroups.ts
@@ -3,25 +3,29 @@ import { IItemGroup } from "./contracts/IItemGroup";
 import { Items } from "./Items";
 
 export const ItemGroups: IdIndex<IItemGroup> = {
-  unknown: {
-    itemIds: [Items.unknown, Items.unknown, Items.unknown, Items.unknown],
-  },
+  unknown: { normalizedName: "unknown", itemIds: [Items.unknown, Items.unknown, Items.unknown, Items.unknown] },
   spectral: {
+    normalizedName: "spectral",
     itemIds: [Items.spectral_nucleus, Items.spectral_heart, Items.spectral_husk],
   },
   light: {
+    normalizedName: "light",
     itemIds: [Items.teachings_of_light, Items.guide_to_light, Items.philosophies_of_light],
   },
   transience: {
+    normalizedName: "transience",
     itemIds: [Items.teachings_of_transience, Items.guide_to_transience, Items.philosophies_of_transience],
   },
   handguard: {
+    normalizedName: "handguard",
     itemIds: [Items.old_handguard, Items.kageuchi_handguard, Items.famed_handguard],
   },
   elegance: {
+    normalizedName: "elegance",
     itemIds: [Items.teachings_of_elegance, Items.guide_to_elegance, Items.philosophies_of_elegance],
   },
   pyro_gem: {
+    normalizedName: "pyro_gem",
     itemIds: [
       Items.agnidus_agate_sliver,
       Items.agnidus_agate_fragment,
@@ -30,6 +34,7 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   geo_gem: {
+    normalizedName: "geo_gem",
     itemIds: [
       Items.prithiva_topaz_sliver,
       Items.prithiva_topaz_fragment,
@@ -38,6 +43,7 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   cryo_gem: {
+    normalizedName: "cryo_gem",
     itemIds: [
       Items.shivada_jade_sliver,
       Items.shivada_jade_fragment,
@@ -46,6 +52,7 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   electro_gem: {
+    normalizedName: "electro_gem",
     itemIds: [
       Items.vajrada_amethyst_sliver,
       Items.vajrada_amethyst_fragment,
@@ -54,6 +61,7 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   hydro_gem: {
+    normalizedName: "hydro_gem",
     itemIds: [
       Items.varunada_lazurite_sliver,
       Items.varunada_lazurite_fragment,
@@ -62,6 +70,7 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   anemo_gem: {
+    normalizedName: "anemo_gem",
     itemIds: [
       Items.vayuda_turquoise_sliver,
       Items.vayuda_turquoise_fragment,
@@ -70,6 +79,7 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   traveler_gem: {
+    normalizedName: "traveler_gem",
     itemIds: [
       Items.brilliant_diamond_sliver,
       Items.brilliant_diamond_fragment,
@@ -78,63 +88,71 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   scroll: {
+    normalizedName: "scroll",
     itemIds: [Items.divining_scroll, Items.sealed_scroll, Items.forbidden_curse_scroll],
   },
   arrowhead: {
+    normalizedName: "arrowhead",
     itemIds: [Items.firm_arrowhead, Items.sharp_arrowhead, Items.weathered_arrowhead],
   },
   treasure_hoarder_insignia: {
+    normalizedName: "treasure_hoarder_insignia",
     itemIds: [Items.treasure_hoarder_insignia, Items.silver_raven_insignia, Items.golden_raven_insignia],
   },
-  mask: {
-    itemIds: [Items.damaged_mask, Items.stained_mask, Items.ominous_mask],
-  },
+  mask: { normalizedName: "mask", itemIds: [Items.damaged_mask, Items.stained_mask, Items.ominous_mask] },
   fatui_insignia: {
+    normalizedName: "fatui_insignia",
     itemIds: [Items.recruits_insignia, Items.sergeants_insignia, Items.lieutenants_insignia],
   },
   nectar: {
+    normalizedName: "nectar",
     itemIds: [Items.whopperflower_nectar, Items.shimmering_nectar, Items.energy_nectar],
   },
   slime: {
+    normalizedName: "slime",
     itemIds: [Items.slime_condensate, Items.slime_secretions, Items.slime_concentrate],
   },
-  chaos: {
-    itemIds: [Items.chaos_device, Items.chaos_circuit, Items.chaos_core],
-  },
-  horn: {
-    itemIds: [Items.heavy_horn, Items.black_bronze_horn, Items.black_crystal_horn],
-  },
+  chaos: { normalizedName: "chaos", itemIds: [Items.chaos_device, Items.chaos_circuit, Items.chaos_core] },
+  horn: { normalizedName: "horn", itemIds: [Items.heavy_horn, Items.black_bronze_horn, Items.black_crystal_horn] },
   bone_shard: {
+    normalizedName: "bone_shard",
     itemIds: [Items.fragile_bone_shard, Items.sturdy_bone_shard, Items.fossilized_bone_shard],
   },
   sacrificial_knife: {
+    normalizedName: "sacrificial_knife",
     itemIds: [Items.hunters_sacrificial_knife, Items.agents_sacrificial_knife, Items.inspectors_sacrificial_knife],
   },
   mist_grass: {
+    normalizedName: "mist_grass",
     itemIds: [Items.mist_grass_pollen, Items.mist_grass, Items.mist_grass_wick],
   },
   ley_line: {
+    normalizedName: "ley_line",
     itemIds: [Items.dead_ley_line_branch, Items.dead_ley_line_leaves, Items.ley_line_sprouts],
   },
   ballad: {
+    normalizedName: "ballad",
     itemIds: [Items.teachings_of_ballad, Items.guide_to_ballad, Items.philosophies_of_ballad],
   },
   freedom: {
+    normalizedName: "freedom",
     itemIds: [Items.teachings_of_freedom, Items.guide_to_freedom, Items.philosophies_of_freedom],
   },
-  gold: {
-    itemIds: [Items.teachings_of_gold, Items.guide_to_gold, Items.philosophies_of_gold],
-  },
+  gold: { normalizedName: "gold", itemIds: [Items.teachings_of_gold, Items.guide_to_gold, Items.philosophies_of_gold] },
   resistance: {
+    normalizedName: "resistance",
     itemIds: [Items.teachings_of_resistance, Items.guide_to_resistance, Items.philosophies_of_resistance],
   },
   diligence: {
+    normalizedName: "diligence",
     itemIds: [Items.teachings_of_diligence, Items.guide_to_diligence, Items.philosophies_of_diligence],
   },
   prosperity: {
+    normalizedName: "prosperity",
     itemIds: [Items.teachings_of_prosperity, Items.guide_to_prosperity, Items.philosophies_of_prosperity],
   },
   dandelion_gladiator: {
+    normalizedName: "dandelion_gladiator",
     itemIds: [
       Items.fetters_of_the_dandelion_gladiator,
       Items.chains_of_the_dandelion_gladiator,
@@ -143,6 +161,7 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   decarabians: {
+    normalizedName: "decarabians",
     itemIds: [
       Items.tile_of_decarabians_tower,
       Items.debris_of_decarabians_city,
@@ -151,6 +170,7 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   aerosiderite: {
+    normalizedName: "aerosiderite",
     itemIds: [
       Items.grain_of_aerosiderite,
       Items.piece_of_aerosiderite,
@@ -159,6 +179,7 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   guyun: {
+    normalizedName: "guyun",
     itemIds: [
       Items.luminous_sands_from_guyun,
       Items.lustrous_stone_from_guyun,
@@ -167,6 +188,7 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   mist_elixir: {
+    normalizedName: "mist_elixir",
     itemIds: [
       Items.mist_veiled_lead_elixir,
       Items.mist_veiled_mercury_elixir,
@@ -175,6 +197,7 @@ export const ItemGroups: IdIndex<IItemGroup> = {
     ],
   },
   wolf_tooth: {
+    normalizedName: "wolf_tooth",
     itemIds: [
       Items.boreal_wolfs_milk_tooth,
       Items.boreal_wolfs_cracked_tooth,
--- src/lib/data/contracts/IItemGroup.ts
+++ src/lib/data/contracts/IItemGroup.ts
@@ -1,5 +1,7 @@
 import { IItem } from "./IItem";
+import { INamed } from "./INamed";
 
 export interface IItemGroup {
+  normalizedName: string;
   itemIds: IItem[];
 }
