1986a8fb899643a4925a1925405b7aa09ac9baf0
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-13 15:41:14 +0100

    connect checklist with indexedDb

--- src/composites/useCheckList.ts
+++ src/composites/useCheckList.ts
@@ -1,18 +1,37 @@
 import { ItemWithAmount } from "~/lib/data/entities/ItemWithAmount";
+import { db } from "~/lib/offlineDatabase/db";
 
-export default (dbId: number) => {
+export default (buildId: number) => {
   const items = ref<ItemWithAmount[]>([]);
 
-  const newItem = (item: ItemWithAmount) => {
+  // sync with data from db
+  const loading = ref(true);
+
+  db.builds
+    .get(buildId)
+    .then((builds) => {
+      const completed = builds?.completed;
+      if (completed) items.value = completed;
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+
+  const syncToDb = async () => await db.builds.update(buildId, { completed: toRaw(items.value) });
+
+  const newItem = async (item: ItemWithAmount) => {
     items.value.push(Object.assign({}, item));
+    await syncToDb();
   };
 
-  const setItemAmount = (index: number, amount: number) => {
+  const setItemAmount = async (index: number, amount: number) => {
     items.value[index].amount = amount;
+    await syncToDb();
   };
 
   return {
     items,
+    loading,
     newItem,
     setItemAmount,
   };
