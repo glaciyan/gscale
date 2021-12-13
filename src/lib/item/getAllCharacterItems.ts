fc21d1e68084dace4c10931f14f3783166bfef50
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-13 11:59:57 +0100

    usual items for all items

--- src/lib/item/getAllCharacterItems.ts
+++ src/lib/item/getAllCharacterItems.ts
@@ -2,9 +2,12 @@ import { IBaseCharacter } from "../data/contracts/IBaseCharacter";
 import { ICharacter } from "../data/contracts/ICharacter";
 import { IItem } from "../data/contracts/IItem";
 import { ITraveler } from "../data/contracts/ITraveler";
+import { Items } from "../data/Items";
 import getTravelerBooks from "./getTravelerBooks";
 
 export default (bCharacter: IBaseCharacter): IItem[] => {
+  const usualItems = [Items.crown_of_insight, Items.mora, Items.heros_wit];
+
   if (bCharacter.isTraveler) {
     const char = bCharacter as ITraveler;
 
@@ -23,6 +26,7 @@ export default (bCharacter: IBaseCharacter): IItem[] => {
       ...books,
       ...char.normalTalentCommonGroup.itemIds,
       char.normalTalentBoss,
+      ...usualItems,
     ];
   } else {
     const char = bCharacter as ICharacter;
@@ -34,6 +38,7 @@ export default (bCharacter: IBaseCharacter): IItem[] => {
       ...char.gemGroup.itemIds,
       ...char.bookGroup.itemIds,
       char.weekly,
+      ...usualItems,
     ];
   }
 };
