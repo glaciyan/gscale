75a71c0b97df81278ca9188e4aaf26f81c32272c
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-14 11:24:13 +0100

    partial item style

--- src/components/ItemCardClickable.vue
+++ src/components/ItemCardClickable.vue
@@ -17,6 +17,7 @@ const partial = computed(() => props.state === ItemCheckState.Partial);
       :class="[
         'ring-inset hover:ring-2 transition-all absolute inset-0 rounded-md ring-green-400 flex items-center justify-center',
         { 'ring !hover:ring bg-black/30 backdrop-filter backdrop-saturate-0 text-green-400': done },
+        { 'ring-2 hover:(ring-green-400) ring-yellow-200': partial },
       ]"
     >
       <FireIcon v-if="done" />
