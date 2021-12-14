4ace5b05088f126a80ba93cd71da7f099cd44c24
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-14 14:27:06 +0100

    setup lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
-  extends: ["plugin:vue/vue3-recommended", "prettier"],
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
     "vue/attribute-hyphenation": [
       "error",
