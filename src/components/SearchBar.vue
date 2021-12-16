9d5d7bf6cefc57e9839e70634a5c3976ee6eb8ae
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-16 12:02:13 +0100

    search bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import SearchIcon from "./icons/SearchIcon.vue";
+defineProps<{ modelValue: string; placeholder?: string }>();
+defineEmits(["update:modelValue"]);
+
+const searchBar = ref<any>(null);
+const focus = () => {
+  searchBar.value.focus();
+};
+</script>
+
+<template>
+  <div
+    class="
+      rounded-lg
+      flex
+      bg-dark-400
+      shadow-md
+      mb-6
+      w-full
+      transition-shadow
+      overflow-hidden
+      items-center
+      focus-within:ring-2
+    "
+    @click="focus"
+  >
+    <SearchIcon class="mx-4 text-light-ternary" />
+    <input
+      ref="searchBar"
+      :value="modelValue"
+      class="bg-transparent placeholder-light-ternary h-[42px] text-light-important w-full pr-4 focus:outline-none"
+      :placeholder="placeholder"
+      @input="$emit('update:modelValue', ($event.target as any).value)"
+    />
+  </div>
+</template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,10 +2,14 @@
 import CharacterCard from "~/components/character/CharacterCard.vue";
 import { charactersViewModel } from "~/lib/viewmodel/CharactersViewModel";
 import Container from "~/components/PageContainer";
+import SearchBar from "~/components/SearchBar.vue";
+
+const value = ref("");
 </script>
 
 <template>
   <Container>
+    <SearchBar v-model="value" placeholder="Search for Names, Materials, Elements, etc..." />
     <div w:grid="cols-2 sm:cols-3 lg:cols-4 xl:cols-5" w:gap="5 <sm:2" class="grid">
       <CharacterCard v-for="character in charactersViewModel" :key="character.normalizedName" :character="character" />
     </div>
