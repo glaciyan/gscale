ea6a8523a56d7ea38c6a1f2e11c9f06ef1c87c31
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-16 14:46:16 +0100

    colorful search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,28 +1,26 @@
 <script setup lang="ts">
+import useRandomElement from "~/composites/useRandomElement";
 import SearchIcon from "./icons/SearchIcon.vue";
 defineProps<{ modelValue: string; placeholder?: string }>();
 defineEmits(["update:modelValue"]);
 
 const searchBar = ref<any>(null);
-const focus = () => {
+
+const { element, pickNew } = useRandomElement();
+
+const handleClick = () => {
+  pickNew();
   searchBar.value.focus();
 };
 </script>
 
 <template>
   <div
-    class="
-      rounded-lg
-      flex
-      bg-dark-400
-      shadow-md
-      w-full
-      transition-shadow
-      overflow-hidden
-      items-center
-      focus-within:ring-2
-    "
-    @click="focus"
+    :class="[
+      'rounded-lg flex bg-dark-400 shadow-md w-full transition-shadow overflow-hidden items-center focus-within:ring-2 ring-opacity-70',
+      `ring-genshin-element-${element}`,
+    ]"
+    @click="handleClick"
   >
     <SearchIcon class="mx-4 text-light-ternary" />
     <input
