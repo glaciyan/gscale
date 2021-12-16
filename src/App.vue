33ede280e37fa61c808ac4a7da09a4f9cee0b1ca
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-16 10:41:41 +0100

    footer component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import VercelLogo from "~/assets/powered-by-vercel.svg?url";
 import TheHeader from "~/components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
 </script>
 
 <template>
@@ -9,14 +9,7 @@ import TheHeader from "~/components/TheHeader.vue";
   <main class="min-h-screen">
     <RouterView />
   </main>
-  <footer>
-    <hr />
-    <span>footer</span>
-    <a href="https://vercel.com?utm_source=gscale&utm_campaign=oss">
-      <img :src="VercelLogo" width="212" height="44" alt="Powered by Vercel" />
-    </a>
-    <a rel="noopener" href="https://github.com/glaciyan/gscale-amber" target="_blank">Github</a>
-  </footer>
+  <TheFooter />
 </template>
 
 <style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import VercelLogo from "~/assets/powered-by-vercel.svg?url";
+</script>
+
+<template>
+  <footer>
+    <hr />
+    <span>footer</span>
+    <a href="https://vercel.com?utm_source=gscale&utm_campaign=oss">
+      <img :src="VercelLogo" width="212" height="44" alt="Powered by Vercel" />
+    </a>
+    <a rel="noopener" href="https://github.com/glaciyan/gscale-amber" target="_blank">Github</a>
+  </footer>
+</template>
