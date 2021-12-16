365b6c7c1eddcbd67eec06104f9bcfef921e4450
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-16 15:17:52 +0100

    scroll to top if last build is deleted

--- src/pages/Builds.vue
+++ src/pages/Builds.vue
@@ -25,15 +25,22 @@ const hasBuilds = computed(() => {
   if (builds.value?.length) return builds.value?.length > 0;
   return false;
 });
+
+const handleBuildDelete = () => {
+  if (builds.value?.length && builds.value?.length <= 1) {
+    window.scrollTo({ top: 0 });
+    builds.value = [];
+  }
+};
 </script>
 
 <template>
   <Container size="2xl">
     <div v-if="hasBuilds" w:grid="gap-5 cols-2 <sm:cols-1" class="grid">
-      <CharacterBuildPreview v-for="build in builds" :key="build.id" :build="build" />
+      <CharacterBuildPreview v-for="build in builds" :key="build.id" :build="build" @deleted="handleBuildDelete" />
     </div>
     <Center v-else>
-      <div class="rounded-lg flex flex-col bg-dark-400 w-max py-4 px-6 items-center justify-center">
+      <div class="rounded-lg flex flex-col bg-dark-400 shadow-md w-max py-4 px-6 items-center justify-center">
         <div class="m-2 text-6xl">?</div>
         <p>You don't have any builds.</p>
         <GLink to="/" isRouter><Button :element="element" class="mt-2">Create one</Button></GLink>
