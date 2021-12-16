a7b7216fd54e906a75c8e7aee04f188335b9ad30
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-16 14:01:34 +0100

    fix character card

--- src/components/character/CharacterCard.vue
+++ src/components/character/CharacterCard.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { RouterLink } from "vue-router";
-import { ICharacterViewModel } from "~/lib/data/contracts/ICharacterViewModel";
 import { useIntersection } from "../../composites/useIntersection";
 import RarityStars from "../RarityStars.vue";
 import Image from "../GImage.vue";
+import { IBaseCharacter } from "~/lib/data/contracts/IBaseCharacter";
 
 const props = defineProps<{
-  character: ICharacterViewModel;
+  character: IBaseCharacter;
 }>();
 
 const elementTextColor = `text-genshin-element-${props.character.element.normalizedName}`;
@@ -21,11 +21,18 @@ const isVisible = useIntersection(image);
 <template>
   <div :class="['shadow-md transition-shadow hover:ring focus-within:ring rounded-md overflow-hidden', hoverRingColor]">
     <RouterLink :to="`/build/${character.normalizedName}`" draggable="false" :title="character.name">
-      <div ref="image" class="flex bg-dark-600 relative items-center justify-center aspect-w-8 aspect-h-5">
-        <Image v-if="isVisible" type="characterCard" :name="character.normalizedName" width="240" height="150" />
+      <div ref="image" class="bg-dark-600 relative aspect-w-8 aspect-h-5">
+        <Image
+          v-if="isVisible"
+          class="h-full w-full"
+          type="characterCard"
+          :name="character.normalizedName"
+          width="240"
+          height="150"
+        />
       </div>
-      <div class="bg-dark-400 leading-snug py-3 px-4 <sm:(py-2 px-3)">
-        <div class="flex flex-wrap flex-row items-center <sm:(flex-col-reverse items-start -mb-0.5)">
+      <div class="bg-dark-400 leading-snug py-3 px-4 <sm:(py-2 px-3) ">
+        <div class="flex flex-wrap flex-row items-center <sm:(flex-col-reverse items-start -mb-0.5) ">
           <div class="mr-1">
             <span :class="elementTextColor">{{ character.element.name }}</span>
             {{ " " }}
