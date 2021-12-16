6f8d5e0e3c303300414a64cb27faacfc5ccc6258
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-16 15:00:09 +0100

    helper for no results

---
+++ src/components/MockSearch.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import SearchIcon from "./icons/SearchIcon.vue";
+
+defineProps<{ term: string }>();
+defineEmits(["click"]);
+</script>
+
+<template>
+  <div
+    class="
+      rounded-lg
+      cursor-pointer
+      flex
+      bg-dark-400
+      h-[42px]
+      shadow-md
+      w-full w-max
+      transition-shadow
+      overflow-hidden
+      items-center
+    "
+    :title="`Search for ${term}`"
+    @click="$emit('click', term)"
+  >
+    <SearchIcon class="mx-4 text-light-ternary" />
+    <p class="mr-8">{{ term }}</p>
+  </div>
+</template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,6 +4,7 @@ import { charactersViewModel } from "~/lib/viewmodel/CharactersViewModel";
 import Container from "~/components/PageContainer";
 import SearchBar from "~/components/SearchBar.vue";
 import useCharacterSearch from "~/composites/useCharacterSearch";
+import MockSearch from "~/components/MockSearch.vue";
 
 const search = ref("");
 const { result: filteredCharacters, noResults: noSearchResults } = useCharacterSearch(search, charactersViewModel);
@@ -15,12 +16,31 @@ const characters = computed(() => {
     return charactersViewModel;
   }
 });
+
+const mockSuggestClick = (newVal: string) => {
+  search.value = newVal;
+};
 </script>
 
 <template>
   <Container>
-    <SearchBar v-model="search" class="mb-6 <sm:mb-2" placeholder="Search for Names, Materials, Elements, etc..." />
-    <p v-if="noSearchResults">No Results</p>
+    <SearchBar
+      v-model.trim="search"
+      class="mb-6 <sm:mb-2"
+      placeholder="Search for Names, Materials, Elements, etc..."
+    />
+    <template v-if="noSearchResults">
+      <p class="text-light-normal">
+        No results for "<span class="text-light-important">{{ search }} </span>"
+      </p>
+      <p class="mt-2">Try someting like:</p>
+      <div class="space-y-2 my-2">
+        <MockSearch term="Ganyu" @click="mockSuggestClick" />
+        <MockSearch term="Pyro" @click="mockSuggestClick" />
+        <MockSearch term="Windwheel Aster" @click="mockSuggestClick" />
+        <MockSearch term="Bow | Claymore" @click="mockSuggestClick" />
+      </div>
+    </template>
     <div v-else w:grid="cols-2 sm:cols-3 lg:cols-4 xl:cols-5" w:gap="5 <md:2" class="grid">
       <CharacterCard v-for="character in characters" :key="character.normalizedName" :character="character" />
     </div>
