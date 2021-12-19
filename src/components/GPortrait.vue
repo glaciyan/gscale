56ecd0e729992779db65bd841ed68e7439a853fe
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-19 09:22:56 +0100

    show weapon images

--- src/components/GPortrait.vue
+++ src/components/GPortrait.vue
@@ -2,14 +2,20 @@
 import RarityStars from "./RarityStars.vue";
 import Image from "./GImage.vue";
 
-const props = defineProps<{ normalizedName: string; rarity?: 4 | 5 }>();
-const mugshotRounding = ["rounded-t-md", { "rounded-b-md": props.rarity === undefined }];
+const props = defineProps<{ normalizedName: string; rarity?: number; weapon?: boolean }>();
+const rounding = ["rounded-t-md", { "rounded-b-md": props.rarity === undefined }];
 </script>
 
 <template>
   <div class="rounded-md bg-dark-500 w-[100px]">
-    <div :class="['bg-dark-500', mugshotRounding]">
-      <Image :class="mugshotRounding" type="characterMugshot" :name="normalizedName" width="100" height="100" />
+    <div :class="['bg-dark-500', rounding]">
+      <Image
+        :class="[rounding, { '': weapon }]"
+        :type="weapon ? 'weapon' : 'characterMugshot'"
+        :name="normalizedName"
+        width="100"
+        height="100"
+      />
     </div>
     <div v-if="rarity" class="rounded-b-md flex bg-dark-400 border-0 py-0.5 items-center justify-center">
       <RarityStars :rarity="rarity" />
--- src/components/weapons/WeaponCard.vue
+++ src/components/weapons/WeaponCard.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { IWeapon } from "~/lib/data/contracts/IWeapon";
+import GPortrait from "../GPortrait.vue";
 
 defineProps<{ weapon: IWeapon }>();
 </script>
 
 <template>
-  <div>{{ weapon.name }}</div>
+  <div>
+    <p>{{ weapon.name }}</p>
+    <GPortrait :normalizedName="weapon.normalizedName" :rarity="weapon.rarity" weapon />
+  </div>
 </template>
