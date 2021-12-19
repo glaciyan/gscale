cbe08eebbe4db7caa3eb970165a14affe8201453
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-19 08:54:35 +0100

    display all weapons

---
+++ src/components/weapons/WeaponCard.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { IWeapon } from "~/lib/data/contracts/IWeapon";
+
+defineProps<{ weapon: IWeapon }>();
+</script>
+
+<template>
+  <div>{{ weapon.name }}</div>
+</template>
---
+++ src/pages/Weapons.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PageContainer from "~/components/PageContainer";
+import WeaponCard from "~/components/weapons/WeaponCard.vue";
+import { weaponsViewModel } from "~/lib/viewmodel/WeaponsViewModel";
+</script>
+
+<template>
+  <PageContainer>
+    <WeaponCard v-for="weapon in weaponsViewModel" :key="weapon.normalizedName" :weapon="weapon" />
+  </PageContainer>
+</template>
