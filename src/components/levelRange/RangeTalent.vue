b6c92a6b0f848f9c02382d2f683ddf98c982caab
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-31 14:47:36 +0100

    level compensation for selector

--- src/components/levelRange/RangeTalent.vue
+++ src/components/levelRange/RangeTalent.vue
@@ -3,18 +3,39 @@ import type { Component } from "vue";
 import SelectorGroup from "../levelSelector/SelectorGroup.vue";
 import SelectorIconSeperator from "../levelSelector/SelectorIconSeperator.vue";
 import SelectorTalentLevel from "../levelSelector/SelectorTalentLevel.vue";
+import GButton from "../GButton.vue";
 
 defineProps<{ start: number; goal: number; icon: Component; element?: string }>();
 
 defineEmits(["update:start", "update:goal"]);
+
+const [compensating, toggle] = useToggle();
 </script>
 
 <template>
   <SelectorGroup legend="Normal Attack">
-    <SelectorTalentLevel class="rounded-l-md" :modelValue="start" @update:modelValue="$emit('update:start', $event)" />
-    <SelectorIconSeperator :element="element">
-      <component :is="icon" class="-m-0.5" />
-    </SelectorIconSeperator>
-    <SelectorTalentLevel class="rounded-r-md" :modelValue="goal" @update:modelValue="$emit('update:goal', $event)" />
+    <div class="flex shadow-md">
+      <SelectorTalentLevel
+        class="rounded-l-md"
+        :modelValue="start"
+        :compensating="compensating"
+        @update:modelValue="$emit('update:start', $event)"
+      />
+      <SelectorIconSeperator :element="element">
+        <component :is="icon" class="-m-0.5" />
+      </SelectorIconSeperator>
+      <SelectorTalentLevel
+        class="rounded-r-md"
+        :modelValue="goal"
+        :compensating="compensating"
+        @update:modelValue="$emit('update:goal', $event)"
+      />
+    </div>
+    <GButton
+      title="Add 3 to talent"
+      :class="['ml-2 !h-[40px]', { 'text-cyan-400': compensating }]"
+      @click="() => toggle()"
+      >+3</GButton
+    >
   </SelectorGroup>
 </template>
--- src/components/levelSelector/SelectorTalentLevel.vue
+++ src/components/levelSelector/SelectorTalentLevel.vue
@@ -2,7 +2,7 @@
 import SelectorCustom from "../SelectorCustom.vue";
 import { range } from "~/lib/range";
 
-defineProps<{ modelValue: number }>();
+defineProps<{ modelValue: number; compensating: boolean }>();
 defineEmits(["update:modelValue"]);
 </script>
 
@@ -10,13 +10,14 @@ defineEmits(["update:modelValue"]);
   <SelectorCustom
     :modelValue="modelValue"
     :listItems="range(10)"
+    :class="{ 'text-cyan-400': compensating }"
     @update:modelValue="$emit('update:modelValue', $event)"
   >
     <template #button="{ value }">
-      {{ value }}
+      {{ compensating ? value + 3 : value }}
     </template>
     <template #item="{ option }">
-      <span class="mr-2">{{ option }}</span>
+      <span class="mr-2">{{ compensating ? option + 3 : option }}</span>
     </template>
   </SelectorCustom>
 </template>
