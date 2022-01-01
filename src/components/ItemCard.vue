999588d5a842b020058b07ae5888cd0e2228f773
glaciyan <30321316+glaciyan@users.noreply.github.com>
2022-01-01 11:34:41 +0100

    replace titles with tooltip in itemcard

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -9,7 +9,10 @@ const amount = computed(() => millify(props.itemWithAmount.amount));
 </script>
 
 <template>
-  <div class="rounded flex flex-col bg-dark-400 m-1 shadow w-max items-center overflow-hidden">
+  <div
+    v-tooltip="`${itemWithAmount.amount.toLocaleString()} ${itemWithAmount.item.name}`"
+    class="rounded flex flex-col bg-dark-400 m-1 shadow w-max items-center overflow-hidden"
+  >
     <div class="h-12 p-1 w-12">
       <Image
         :class="['h-full object-contain w-full', { 'filter grayscale': grayscale }]"
@@ -18,9 +21,8 @@ const amount = computed(() => millify(props.itemWithAmount.amount));
         width="40"
         height="40"
         :alt="itemWithAmount.item.name"
-        :title="itemWithAmount.item.name"
       />
     </div>
-    <div class="bg-dark-300 text-center w-full px-1" :title="itemWithAmount.amount.toLocaleString()">{{ amount }}</div>
+    <div class="bg-dark-300 text-center w-full px-1">{{ amount }}</div>
   </div>
 </template>
