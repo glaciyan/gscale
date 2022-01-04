2942188f9ad50b87290f382873ee01b93363a165
glaciyan <30321316+glaciyan@users.noreply.github.com>
2022-01-04 14:12:08 +0100

    getbyid goes by normalizedName

--- src/lib/data/util/getById.ts
+++ src/lib/data/util/getById.ts
@@ -1,8 +1,8 @@
-import { IdIndex } from "../../types/UsingId";
+import { INamed } from "../contracts/INamed";
 
-// Get an object by key or undefined if nothing was found
-export function getById<T>(from: IdIndex<T>, id: string): T | undefined {
-  const matchingKeyObject = Object.entries(from).find(([key]) => key === id);
+// Get an object by normalizedName, undefined if nothing was found
+export function getById<T extends INamed>(from: { [key: string]: T }, id: string): T | undefined {
+  const matchingKeyObject = Object.entries(from).find(([, named]) => named.normalizedName === id);
   if (matchingKeyObject === undefined) return undefined;
 
   const [, value] = matchingKeyObject;
