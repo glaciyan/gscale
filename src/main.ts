38d00ac785edb69d35f720ccfb72b82da63ff821
glaciyan <30321316+glaciyan@users.noreply.github.com>
2022-01-05 11:37:58 +0100

    some neat console stuff

--- src/main.ts
+++ src/main.ts
@@ -18,4 +18,14 @@ tooltip(app, {
   arrowPadding: 10,
 });
 
+console.log(
+  "%cThanks for checking out gscale.cc",
+  "font-size: 1.5rem; font-weight: bold; background-image: url( 'https://www.gscale.cc/favicon.svg' );background-size: 4rem 4rem; background-repeat: no-repeat; background-position: center left; padding: 2rem 0px 2rem 5rem"
+);
+
+console.log(
+  "%cgscale.cc is open source https://github.com/glaciyan/gscale",
+  "font-size: 1.3rem; font-weight: bold; color: #60a5fa;"
+);
+
 app.mount("#app");
