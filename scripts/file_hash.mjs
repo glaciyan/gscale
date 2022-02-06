68d14170346d0dd3ad29b3bdfc9b75d09ebfe683
glaciyan <30321316+glaciyan@users.noreply.github.com>
2022-02-06 12:14:08 +0100

    include postfix in file hashes

--- scripts/file_hash.mjs
+++ scripts/file_hash.mjs
@@ -4,19 +4,19 @@ import path from "path";
 
 export const getAssetHash = (file) => createHash("sha256").update(fs.readFileSync(file)).digest("hex").substring(0, 8);
 
-export const getAssetName = (file) => {
+export const getAssetName = (file, postfix) => {
   const dir = path.dirname(file);
 
   const extname = path.extname(file);
   const baseName = path.basename(file, extname);
   const hash = getAssetHash(file);
 
-  return path.join(dir, `${baseName}.${hash}${extname}`);
+  return path.join(dir, `${baseName}.${hash}.${postfix}${extname}`);
 };
 
 const hashMap = new Map();
 
-const addHashesToFilesInDir = (dir) => {
+const addHashesToFilesInDir = (dir, postfix) => {
   const withPath = (fileName) => path.join(dir, fileName);
 
   const fileNames = fs.readdirSync(dir);
@@ -25,7 +25,7 @@ const addHashesToFilesInDir = (dir) => {
 
     return {
       oldPath: withPath(fileName),
-      newPath: getAssetName(withPath(fileName)),
+      newPath: getAssetName(withPath(fileName), postfix),
       hash: getAssetHash(withPath(fileName)),
       name,
     };
@@ -34,23 +34,25 @@ const addHashesToFilesInDir = (dir) => {
   renames.forEach(({ oldPath, newPath, name, hash }) => {
     fs.renameSync(oldPath, newPath);
 
-    const hashObj = hashMap.get(name);
+    const mapName = `${name}_${postfix}`;
+
+    const hashObj = hashMap.get(mapName);
     if (hashObj) {
       if (hashObj.png && oldPath.endsWith("webp")) {
-        hashMap.set(name, Object.assign(hashObj, { webp: hash }));
+        hashMap.set(mapName, Object.assign(hashObj, { webp: hash }));
       } else if (hashObj.webp && oldPath.endsWith("png")) {
-        hashMap.set(name, Object.assign(hashObj, { png: hash }));
+        hashMap.set(mapName, Object.assign(hashObj, { png: hash }));
       }
     } else {
-      hashMap.set(name, Object.fromEntries([[path.extname(oldPath).substring(1), hash]]));
+      hashMap.set(mapName, Object.fromEntries([[path.extname(oldPath).substring(1), hash]]));
     }
   });
 };
 
 async function main() {
-  addHashesToFilesInDir("./public/images/characters/card");
-  addHashesToFilesInDir("./public/images/characters/mugshot");
-  addHashesToFilesInDir("./public/images/materials");
+  addHashesToFilesInDir("./public/images/characters/card", "card");
+  addHashesToFilesInDir("./public/images/characters/mugshot", "mugshot");
+  addHashesToFilesInDir("./public/images/materials", "material");
 
   fs.writeFileSync("./src/assets/image_hashes.json", JSON.stringify(Object.fromEntries(hashMap)));
 }
