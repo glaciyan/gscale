3898964031c08c471775426161bfe4c596c0b084
glaciyan <30321316+glaciyan@users.noreply.github.com>
2022-02-06 11:49:55 +0100

    write image hashes json

--- scripts/file_hash.mjs
+++ scripts/file_hash.mjs
@@ -14,25 +14,45 @@ export const getAssetName = (file) => {
   return path.join(dir, `${baseName}.${hash}${extname}`);
 };
 
+const hashMap = new Map();
+
 const addHashesToFilesInDir = (dir) => {
   const withPath = (fileName) => path.join(dir, fileName);
 
   const fileNames = fs.readdirSync(dir);
   const renames = fileNames.map((fileName) => {
+    const name = path.basename(fileName, fileName.endsWith("png") ? ".png" : ".webp");
+
     return {
       oldPath: withPath(fileName),
       newPath: getAssetName(withPath(fileName)),
+      hash: getAssetHash(withPath(fileName)),
+      name,
     };
   });
 
-  renames.forEach(({ oldPath, newPath }) => {
+  renames.forEach(({ oldPath, newPath, name, hash }) => {
     fs.renameSync(oldPath, newPath);
-    console.log(oldPath, "->", newPath);
+
+    const hashObj = hashMap.get(name);
+    if (hashObj) {
+      if (hashObj.png && oldPath.endsWith("webp")) {
+        hashMap.set(name, Object.assign(hashObj, { webp: hash }));
+      } else if (hashObj.webp && oldPath.endsWith("png")) {
+        hashMap.set(name, Object.assign(hashObj, { png: hash }));
+      }
+    } else {
+      hashMap.set(name, Object.fromEntries([[path.extname(oldPath).substring(1), hash]]));
+    }
   });
 };
 
 async function main() {
   addHashesToFilesInDir("./public/images/characters/card");
+  addHashesToFilesInDir("./public/images/characters/mugshot");
+  addHashesToFilesInDir("./public/images/materials");
+
+  fs.writeFileSync("./src/assets/image_hashes.json", JSON.stringify(Object.fromEntries(hashMap)));
 }
 
 main();
