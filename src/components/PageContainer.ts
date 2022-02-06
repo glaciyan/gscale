f709e3c23ea8d23d3b0d9f5704f12d04e6552ce7
glaciyan <30321316+glaciyan@users.noreply.github.com>
2022-02-06 19:52:58 +0100

    add missing option for sm

--- src/components/PageContainer.ts
+++ src/components/PageContainer.ts
@@ -9,14 +9,24 @@ export default defineComponent({
       default: "div",
     },
     size: {
-      type: String as PropType<"xl" | "2xl">,
+      type: String as PropType<"xl" | "2xl" | "sm">,
       required: false,
       default: "xl",
     },
   },
   render() {
     // change this into a switch if more are needed
-    const screenSize = this.$props.size === "xl" ? "max-w-screen-xl xlp:px-4" : "max-w-screen-2xl 2xlp:px-4";
+    let screenSize: string;
+    switch (this.$props.size) {
+      case "xl":
+        screenSize = "max-w-screen-xl xlp:px-4";
+        break;
+      case "2xl":
+        screenSize = "max-w-screen-2xl 2xlp:px-4";
+        break;
+      case "sm":
+        screenSize = "max-w-screen-sm px-4";
+    }
 
     const className = `mx-auto ${screenSize} <sm:px-2`;
 
